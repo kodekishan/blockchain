355800d3c7d2f7aad132405d55b864c0bdc5642f
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-07-08 17:47:57 +0530

    Created the class block chain with init method

--- blockchain.py
+++ blockchain.py
@@ -13,8 +13,34 @@ import json
 from flask import Flask, jsonify
 
 #Part 1 - Building a Blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
 
 
 
-#Part 2 - Mining our Blockchain
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
