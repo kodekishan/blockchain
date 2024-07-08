411ccb82de0d7abe6e62e4d9796a3ee4c7a22224
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-07-08 17:52:14 +0530

    Initialising the app from Flask and creating the blockchain

--- blockchain.py
+++ blockchain.py
@@ -67,6 +67,14 @@ class Blockchain:
         return True
         
 
+#Part 2 - Mining our Blockchain
+
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
 
 
 
