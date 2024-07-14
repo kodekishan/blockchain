ee80871a724214294116eb713b3d84bebaed7670
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-07-14 19:47:19 +0530

    Added the app route for GET methods

--- blockchain.py
+++ blockchain.py
@@ -40,7 +40,7 @@ class Blockchain:
         return new_proof
     
     def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True)
+        encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
     def is_chain_valid(self, chain):
@@ -69,16 +69,49 @@ class Blockchain:
 
 # Creating a Web App
 app = Flask(__name__)
+#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 # Creating a Blockchain
 blockchain = Blockchain()
 
 
+# Mining the new block
+@app.route("/mine_block", methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
 
 
+# Getting the full Blockchain
+@app.route("/get_chain", methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
 
 
+# Checking if the Blockchain is valid
+@app.route("/is_valid", methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': "We have a problem, The Blockchain is not valid."}
+    
+    return jsonify(response), 200
 
+# Running the app
+app.run(host='0.0.0.0', port=5000)
 
 
 
