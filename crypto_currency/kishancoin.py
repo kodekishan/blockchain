b70873f1b00fa0e4a5f660077d4b3fddebfc7d2e
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-07-16 16:46:46 +0530

    Added the add transaction, connect node and replace chain method

--- crypto_currency/kishancoin.py
+++ crypto_currency/kishancoin.py
@@ -154,5 +154,46 @@ def is_valid():
   
   return jsonify(response), 200
 
+
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+  json = request.get_json()
+  transaction_keys = ['sender', 'receiver', 'amount']
+  if not all (key in json for key in transaction_keys):
+    return 'Some elements of the transaction are missing', 400
+  index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+  response = {'message': f'This transaction will be added to block {index}'}
+  return jsonify(response), 201
+
+# Decentralizing the Blockchain
+# Connecting new nodes
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+  json = request.get_json()
+  nodes = json.get('node')
+  if nodes is None:
+    return "No node", 400
+  for node in nodes:
+    blockchain.add_node(node)
+  response = {'message': 'All the nodes are now connected. The kishancoin blockchain now contains the following nodes:', 
+              'total_nodes': list(blockchain.nodes)}
+
+  return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route("/replace_chain", methods=['GET'])
+def is_valid():
+  is_chain_replaced = blockchain.replace_chain()
+  if is_chain_replaced:
+    response = {'message': 'The node had different chains so the chain was replaced by the longest one.',
+                'new_chain': blockchain.chain}
+  else:
+    response = {'message': "All good. The chain is the largest one.",
+                'actual_chain': blockchain.chain}
+  
+  return jsonify(response), 200
+      
+
 # Running the app
 app.run(host='0.0.0.0', port=5000)
